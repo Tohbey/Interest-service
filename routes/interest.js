b72cb648c058102ab60cb8b9af4a9991c353976b
tohbey <fafoworatobi25@gmail.com>
2021-10-30 12:14:15 +0100

    router change

--- routes/interest.js
+++ routes/interest.js
@@ -4,9 +4,9 @@ const { Auth } = require('../middlewares/auth')
 
 router.post('/', Auth, controller.interest.createInterest)
 
-router.get('/', Auth, controller.interest.getInterestForUser)
-
 router.get('/', Auth, controller.interest.getAllInterest)
+
+router.get('/me', Auth, controller.interest.getInterestForUser)
 
 router.get('/:id', Auth, controller.interest.getInterest)
 
